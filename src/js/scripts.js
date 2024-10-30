10c640a329e5066687e068e2505fa3583a540859
vitalysementsov <v.sementsov@ratkus.com.ua>
2024-10-30 11:44:12 +0200

    update responsive style

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,37 @@
-document.querySelectorAll('.accordion__header').forEach(button => {
-  button.addEventListener('click', () => {
-    const expanded = button.getAttribute('aria-expanded') === 'true';
-    button.setAttribute('aria-expanded', !expanded);
+const toggleBtn = document.querySelector(".header__toggle-btn");
+const mobMenu = document.querySelector(".mobile-menu");
+
+toggleBtn.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+  mobMenu.classList.toggle("is-open");
+  document.documentElement.classList.toggle("no-scroll");
+});
+
+document.querySelectorAll(".accordion__header").forEach((button) => {
+  button.addEventListener("click", () => {
+    const expanded = button.getAttribute("aria-expanded") === "true";
+    button.setAttribute("aria-expanded", !expanded);
 
     const content = button.nextElementSibling;
-    content.style.display = expanded ? 'none' : 'block';
+    content.style.display = expanded ? "none" : "block";
   });
+});
+
+let lastScrollY = window.scrollY;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > lastScrollY) {
+    header.style.transform = "translateY(-100%)";
+    header.classList.remove("scroll");
+  } else {
+    header.style.transform = "translateY(0)";
+    header.classList.add("scroll");
+  }
+  if (window.scrollY === 0) {
+    header.style.transform = "translateY(0)";
+    header.classList.remove("scroll");
+  }
+
+  lastScrollY = window.scrollY;
 });
