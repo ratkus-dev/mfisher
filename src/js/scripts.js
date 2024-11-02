0ff19d0de9b4f2f7231e0e0ce63f7238d841ef28
vitalysementsov <v.sementsov@ratkus.com.ua>
2024-11-02 17:16:07 +0200

    update page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,8 @@
 import ".././styles/pages/index.scss";
 import "jquery/dist/jquery.js";
 import $ from "jquery";
-import "slick-carousel/slick/slick.js";
+import Swiper from "swiper";
+import "swiper/css";
 
 const toggleBtn = document.querySelector(".header__toggle-btn");
 const mobMenu = document.querySelector(".mobile-menu");
@@ -69,9 +70,65 @@ window.addEventListener("scroll", () => {
   lastScrollY = window.scrollY;
 });
 
-$(".team__list").slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  cssEase: "linear",
+let swiper = new Swiper(".testimonials__list", {
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    1279: {
+      slidesPerView: 2,
+    },
+  },
 });
+
+let expertSwiper = new Swiper(".team__list", {
+  // cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+// $(".testimonials__list").slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   centerMode: true,
+//   variableWidth: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   responsive: [
+//     {
+//       breakpoint: 1279,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     {
+//       breakpoint: 767,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//       },
+//     },
+//   ],
+// });
